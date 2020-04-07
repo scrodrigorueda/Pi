b3eaef6192e022b13a41edc3cb4037781db3d6d2
sc.rodrigo.rueda@gmail.com <sc.rodrigo.rueda@gmail.com>
2020-04-06 23:44:19 -0400

    Se creo el programa para sacar el valor de pi

--- pi_m.py
+++ pi_m.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env Python3
-print("tenes que trabajar en esto loco !!!!")
+import random
+print("ingrese la cantidad de valores:")
+cdv = input()
+def valor_pi(n):
+	c = 0
+	for x in range(n): 	
+		x = random.uniform(0,1)
+		y = random.uniform(0,1)
+		if x**2+y**2 < 1 :
+			c = c + 1
+	v_pi = 4*(c/n)
+	return v_pi	
+print("El valor de pi para esta cantidad de puntos es:")
+print(valor_pi(int(cdv)))
+
+
